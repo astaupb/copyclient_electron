010e7e5904fe2f3817de5d12461945d102adcf42
Michael Koch <m.koch@emkay443.de>
2018-11-28 16:00:20 +0100

    Since main.js didn't differ from Electron defaults, we can probably import the one from the old CC safely here.

--- main.js
+++ main.js
@@ -1,49 +1,189 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+/**
+ * @license LGPL-3.0-or-later
+ * Copyright (c) 2018 Michael Koch <m.koch@emkay443.de>
+ *
+ * This file is part of AStA Copyclient.
+ * AStA Copyclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AStA Copyclient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with AStA Copyclient.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const electron = require('electron');
+const Menu = electron.Menu;
+const Tray = electron.Tray;
+const nativeImage = electron.nativeImage;
+
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+const optionDefinitions = [
+	{
+		name: 'hide',
+		alias: 'h',
+		type: Boolean
+	}
+];
+
+const commandLineArgs = require('command-line-args');
+const options = commandLineArgs(optionDefinitions);
+
+const config = require(path.join(__dirname, 'includes', 'config'));
+let _kiosk = config._kiosk;
+let _locale = config._locale;
+
+
+const l10n = new(require(path.join(__dirname, 'includes', 'l10n')));
+
+var _windowCreated = false;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, tray;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createTray() {
+	var tryImage;
+	var imageFolder = __dirname + '/includes/icons';
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('web/index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+	if (process.platform === 'win32') {
+		trayImage = imageFolder + '/asta.ico';
+	} else {
+		trayImage = imageFolder + '/asta.png';
+	}
+	tray = new Tray(nativeImage.createFromPath(trayImage));
+	var contextMenu = Menu.buildFromTemplate([
+		{
+			label: l10n.getString(56),
+			click:  function() {
+				if (! _windowCreated) {
+					createWindow();
+				} else {
+					mainWindow.show();
+					mainWindow.webContents.executeJavaScript("setupIntervals();");
+				}
+			}
+		},
+		{
+			label: l10n.getString(58),
+			click: function() {
+				mainWindow.webContents.openDevTools();
+			}
+		},
+		{
+			label: l10n.getString(57),
+			click:  function() {
+				app.isQuitting = true;
+				app.quit();
+			}
+		}
+	]);
+	tray.setToolTip(l10n.getString(0));
+	tray.setContextMenu(contextMenu);
+	tray.on('click', function() {
+		if (mainWindow.isVisible()) {
+			mainWindow.hide();
+			mainWindow.webContents.executeJavaScript("unsetIntervals();");
+		} else {
+			if (! _windowCreated) {
+				createWindow();
+			} else {
+				mainWindow.show();
+				mainWindow.webContents.executeJavaScript("setupIntervals();");
+			}
+		}
+	});
 }
+
+function createWindow() {
+	_windowCreated = true;
+
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		minWidth: 800,
+		minHeight: 600,
+		title: l10n.getString(0),
+		icon: "./includes/icons/asta.png"
+	});
+
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	mainWindow.focus();
+
+	mainWindow.on('minimize', function(event) {
+		event.preventDefault();
+		mainWindow.hide();
+		mainWindow.webContents.executeJavaScript("unsetIntervals();");
+	});
+
+	mainWindow.on('close', function(event) {
+		if (! app.isQuitting) {
+			event.preventDefault();
+			mainWindow.hide();
+			mainWindow.webContents.executeJavaScript("unsetIntervals();");
+		}
+
+		return false;
+	});
+
+	// Open the DevTools.
+	//mainWindow.webContents.openDevTools();
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function() {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	});
+}
+
+app.setAppUserModelId("de.upb.asta.copyclient");
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function() {
+	if (! options.hide && ! _windowCreated) {
+		createWindow();
+	}
+	createTray();
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+app.on('window-all-closed', function() {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 })
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('activate', function() {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 })
 
 // In this file you can include the rest of your app's specific main process
