47fc6c2aeafb6dfb9afd53571fb6b1a5f55ae290
Michael Koch <emkay443@mail.upb.de>
2019-10-14 15:13:45 +0200

    Disable default duplex

--- fakeprinter/unix/install_printer.sh
+++ fakeprinter/unix/install_printer.sh
@@ -72,5 +72,6 @@ $SUDO chmod $spool_chmod "$spool_dir"
 $SUDO lpadmin -p "Copyclient" -v "astaprint:$spool_dir" -E -P "$cups_ppd_file"
 $SUDO lpadmin -p "Copyclient" -o "media=A4"
 $SUDO lpoptions -p "Copyclient" -o "ColorModel=Gray"
+$SUDO lpoptions -p "Copyclient" -o "Duplex=None"
 $SUDO lpadmin -d "Copyclient"
 exit 0
