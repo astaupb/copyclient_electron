57652a7c0f8b4b36e1470f81c236e7d0105e0c71
Michael Koch <emkay443@mail.upb.de>
2019-11-19 12:59:43 +0100

    Fix change_config.sh for macOS

--- build/change_config.sh
+++ build/change_config.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 cd -P -- "$(dirname -- "$0")"/..
 
+# inject our stuff into the HTML
+if [[ "$(uname)" == *Darwin* ]]; then
+	sed=gsed
+else
+	sed=sed
+fi
+
 if [[ ! -z "$1" ]]; then
 	if [[ "$1" == "enable_kiosk" ]]; then
-		sed -i "s/_kiosk = false/_kiosk = true/g" includes/config.js
+		$sed -i "s/_kiosk = false/_kiosk = true/g" includes/config.js
 	fi
 	if [[ "$1" == "disable_kiosk" ]]; then
-		sed -i "s/_kiosk = true/_kiosk = false/g" includes/config.js
+		$sed -i "s/_kiosk = true/_kiosk = false/g" includes/config.js
 	fi
 	if [[ "$1" == "enable_starthidden" ]]; then
-		sed -i "s/_startHidden = false/_startHidden = true/g" includes/config.js
+		$sed -i "s/_startHidden = false/_startHidden = true/g" includes/config.js
 	fi
 	if [[ "$1" == "disable_starthidden" ]]; then
-		sed -i "s/_startHidden = true/_startHidden = false/g" includes/config.js
+		$sed -i "s/_startHidden = true/_startHidden = false/g" includes/config.js
 	fi
 	if [[ "$1" == "build_all_linux" ]]; then
 		tmp=$(mktemp)
